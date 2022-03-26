8318dc217a1bbd44db28ee41956c3dfe32062a83
poojab <pbhaumik26@gmail.com>
2022-03-26 16:48:08 +0530

    Add listView.builder to render dynamic chatBubble

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -23,18 +23,16 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            //TODO: Create a dynamic sized list
-            child: ListView(
-              children: [
-                ChatBubble(
-                    alignment: Alignment.centerLeft,
-                    message: "Hello, this is Pooja!"),
-                ChatBubble(
-                    alignment: Alignment.centerRight,
-                    message: "Hello, this is Pooja!"),
-              ],
-            ),
-          ),
+              //TODO: Create a dynamic sized list
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return ChatBubble(
+                        alignment: index % 2 == 0
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        message: "Hello, this is Pooja!");
+                  })),
           ChatInput(),
         ],
       ),
