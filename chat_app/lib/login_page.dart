940aa0f953089b14f5adadfbd559fa01d7b4fca8
poojab <pbhaumik26@gmail.com>
2022-04-15 02:07:28 +0530

    Added url_launcher for login screen UI

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:chat_app/utils/spaces.dart';
-import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -23,6 +23,8 @@ class LoginPage extends StatelessWidget {
 
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  final _mainUrl = "https://poojabhaumik.com";
 
   @override
   Widget build(BuildContext context) {
@@ -92,14 +94,16 @@ class LoginPage extends StatelessWidget {
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
                   )),
               GestureDetector(
-                onTap: () {
-                  //todo: Navigate to browser
+                onTap: () async {
                   print('Link clicked!');
+                  if (!await launch(_mainUrl)) {
+                    throw 'Could not launch this!';
+                  }
                 },
                 child: Column(
                   children: [
                     Text('Find us on'),
-                    Text('https://poojabhaumik.com'),
+                    Text(_mainUrl),
                   ],
                 ),
               )
