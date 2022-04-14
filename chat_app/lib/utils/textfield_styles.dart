1e83bb2a105f65e8d68e091b6768988ee844f1e4
poojab <pbhaumik26@gmail.com>
2022-04-15 01:53:11 +0530

    Add Google font for login text style

--- chat_app/lib/utils/textfield_styles.dart
+++ chat_app/lib/utils/textfield_styles.dart
@@ -1,6 +1,8 @@
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
 
-class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+class ThemeTextStyle {
+  static TextStyle loginTextFieldStyle =
+      GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
 }
