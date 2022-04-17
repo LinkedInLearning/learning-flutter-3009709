34bd97364551b08acdc2ae20249c8ef879750ee9
poojab <pbhaumik26@gmail.com>
2022-04-18 01:37:10 +0530

    Add mock list and supply to listview.builder()

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -4,10 +4,28 @@ import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
 
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'First text'),
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231442,
+        id: '1',
+        text: 'Second text',
+        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
+    ChatMessageEntity(
+      author: Author(userName: 'jane'),
+      createdAt: 2131234242,
+      id: '1',
+      text: 'Third text',
+    )
+  ];
 
-  //TODO: Create mock list
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
@@ -30,17 +48,13 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
               child: ListView.builder(
-                  itemCount: 10,
+                  itemCount: _messages.length,
                   itemBuilder: (context, index) {
                     return ChatBubble(
-                        alignment: index % 2 == 0
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        entity: ChatMessageEntity(
-                            id: '1234',
-                            text: 'Hello this is Pooja!!!!!',
-                            createdAt: DateTime.now().millisecondsSinceEpoch,
-                            author: Author(userName: 'poojab26')));
+                        alignment: _messages[index].author.userName == 'pooja'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        entity: _messages[index]);
                   })),
           ChatInput(),
         ],
