8f49847dcfdb1239e7f68e4f1967375e6d1d1de6
poojab <pbhaumik26@gmail.com>
2022-04-18 02:17:17 +0530

    Load chat messages from JSOna nd assign it to ListView

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,30 +1,44 @@
+import 'dart:convert';
+
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
 
-  List<ChatMessageEntity> _messages = [
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231242,
-        id: '1',
-        text: 'First text'),
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231442,
-        id: '1',
-        text: 'Second text',
-        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
-    ChatMessageEntity(
-      author: Author(userName: 'jane'),
-      createdAt: 2131234242,
-      id: '1',
-      text: 'Third text',
-    )
-  ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  //initiate state of messages
+  List<ChatMessageEntity> _messages = [];
+
+  _loadInitialMessages() async {
+    final response = await rootBundle.loadString('assets/mock_messages.json');
+
+    final List<dynamic> decodedList = jsonDecode(response) as List;
+
+    final List<ChatMessageEntity> _chatMessages = decodedList.map((listItem) {
+      return ChatMessageEntity.fromJson(listItem);
+    }).toList();
+
+    print(_chatMessages.length);
+
+    //final state of the messages
+    setState(() {
+      _messages = _chatMessages;
+    });
+  }
+
+  @override
+  void initState() {
+    _loadInitialMessages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- chat_app/lib/models/chat_message_entity.dart
+++ chat_app/lib/models/chat_message_entity.dart
@@ -11,10 +11,22 @@ class ChatMessageEntity {
       required this.createdAt,
       this.imageUrl,
       required this.author});
+
+  factory ChatMessageEntity.fromJson(Map<String, dynamic> json) {
+    return ChatMessageEntity(
+        text: json['text'],
+        id: json['id'],
+        createdAt: json['createdAt'],
+        author: Author.fromJson(json['author']));
+  }
 }
 
 class Author {
   String userName;
 
   Author({required this.userName});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(userName: json['username']);
+  }
 }
