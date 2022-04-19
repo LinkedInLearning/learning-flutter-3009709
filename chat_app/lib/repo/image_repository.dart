41c9708c066ebdb0fcca0f26fc9e2f77c25fda96
poojab <pbhaumik26@gmail.com>
2022-04-20 02:40:51 +0530

    Handle known exceptions

--- chat_app/lib/repo/image_repository.dart
+++ chat_app/lib/repo/image_repository.dart
@@ -1,26 +1,38 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:chat_app/models/image_model.dart';
 import 'package:http/http.dart' as http;
 
 class ImageRepository {
   Future<List<PixelfordImage>> getNetworkImages() async {
-    var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
+    try {
+      var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
 
-    final response = await http.get(endpointUrl);
+      final response = await http.get(endpointUrl);
 
-    if (response.statusCode == 200) {
-      final List<dynamic> decodedList = jsonDecode(response.body) as List;
+      if (response.statusCode == 200) {
+        final List<dynamic> decodedList = jsonDecode(response.body) as List;
 
-      final List<PixelfordImage> _imageList = decodedList.map((listItem) {
-        return PixelfordImage.fromJson(listItem);
-      }).toList();
+        final List<PixelfordImage> _imageList = decodedList.map((listItem) {
+          return PixelfordImage.fromJson(listItem);
+        }).toList();
 
-      print(_imageList[0].urlFullSize);
-      return _imageList;
-    } else {
-      //TODO: Handle specific exceptions
-      throw Exception('API not successful!');
+        print(_imageList[0].urlFullSize);
+        return _imageList;
+      } else {
+        //TODO: Handle specific exceptions
+        throw Exception('API not successful!');
+      }
+    } on SocketException {
+      throw Exception('No internet connection :(');
+    } on HttpException {
+      throw Exception('Couldnt retrieve the images! Sorry!');
+    } on FormatException {
+      throw Exception('Bad response format!');
+    } catch (e) {
+      print(e);
+      throw Exception('Unknown error');
     }
   }
 }
--- chat_app/lib/widgets/picker_body.dart
+++ chat_app/lib/widgets/picker_body.dart
@@ -22,8 +22,6 @@ class NetworkImagePickerBody extends StatelessWidget {
             return GridView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                //TODO: Add a Gesture Detector to register clicks on each image
-
                 return GestureDetector(
                     onTap: () {
                       onImageSelected(snapshot.data![index].urlSmallSize);
@@ -34,6 +32,11 @@ class NetworkImagePickerBody extends StatelessWidget {
                   crossAxisSpacing: 2,
                   mainAxisSpacing: 2,
                   maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5),
+            );
+          } else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('This is the error: ${snapshot.error}'),
             );
           }
 
