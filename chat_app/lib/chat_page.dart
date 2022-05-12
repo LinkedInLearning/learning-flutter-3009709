4e56de7dce585a7a552c8c9c0035870265581eab
poojab <pbhaumik26@gmail.com>
2022-05-13 02:21:19 +0530

    Add todos

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
+  //TODO Create reusable widgets for Chat Bubble and Chat Input
   @override
   Widget build(BuildContext context) {
     return Scaffold(
